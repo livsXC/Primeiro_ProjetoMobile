72dd26d3ea880c81591a21a23f57942a1531aaea
livsXC <liviasouzaxc@gmail.com>
2024-05-23 20:31:40 -0300

    Finalizando App

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+public resultado!:string;
+public base!:number;
+public altura!:number;
 
   constructor() {}
 
+  calcularArea(){
+    this.resultado=" A área do triângulo é de "+(this.base*this.altura)/2;
+}
 }
